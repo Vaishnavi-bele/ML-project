5f9ed30b16a83f9cb66c4430bbf5e301e1623442
vaishnavibele <vaishnavibele693@gmail.com>
2026-03-27 15:08:22 +0530

    updated exception

--- src/exception.py
+++ src/exception.py
@@ -1,13 +1,22 @@
 import sys
-def error_message_detail(error,error_detail : sys):
-    _,_,exc_tb = error_detail.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occured in Python script name[{0}] line number [{1}] error message[{2}]".format(file_name,exc_tb.tb_lineno,str(error))
-    return error_message
 
-class CustomEception(Exception):
-    def __init__(self, error_message, error_detail:sys):
-        super().__init__(error_message)
-        self.error_message = error_message_detail(error_message,error_detail=error_detail)
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+
+    if exc_tb is not None:
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_number = exc_tb.tb_lineno
+    else:
+        file_name = "Unknown"
+        line_number = "Unknown"
+
+    return f"Error occurred in script [{file_name}] at line [{line_number}] : {str(error)}"
+
+
+class CustomException(Exception):
+    def __init__(self, error, error_detail: sys):
+        super().__init__(str(error))
+        self.error_message = error_message_detail(error, error_detail)
+
     def __str__(self):
         return self.error_message
