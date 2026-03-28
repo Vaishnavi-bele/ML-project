a926d21fc4bfc2172702cb19905df926524cbe7a
vaishnavibele <vaishnavibele693@gmail.com>
2026-03-28 22:33:09 +0530

    data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -45,6 +45,6 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    train_data,test_data = obj.initiate_data_ingestion()
+    obj.initiate_data_ingestion()
     
     
