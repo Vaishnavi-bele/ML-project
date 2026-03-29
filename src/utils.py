90bffacb181519133aad21fe355b6986f085205d
vaishnavibele <vaishnavibele693@gmail.com>
2026-03-29 22:24:19 +0530

    model trainer

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,8 @@ import numpy as np
 import pandas as pd
 
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
 
 def save_object(file_path,obj):
     try:
@@ -18,3 +20,30 @@ def save_object(file_path,obj):
     
     except Exception as e:
         raise CustomException(e,sys)
+
+from sklearn.metrics import r2_score
+
+def evaluate_models(x_train, y_train, x_test, y_test, models, param):
+    try:
+        report = {}
+
+        for model_name, model in models.items():
+
+            # Train model
+            model.fit(x_train, y_train)
+
+            # Predictions
+            y_train_pred = model.predict(x_train)
+            y_test_pred = model.predict(x_test)
+
+            # Scores
+            train_score = r2_score(y_train, y_train_pred)
+            test_score = r2_score(y_test, y_test_pred)
+
+            # Store only test score
+            report[model_name] = test_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
